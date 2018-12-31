74d821a0d702f566e2987ca18b444df3054f3575
Pratibha Kharel <pk66@cs.washington.edu>
2018-12-30 16:54:33 -0800

    keygen code completion

--- src/main/java/pratibha/co/Application.java
+++ src/main/java/pratibha/co/Application.java
@@ -1,14 +1,21 @@
 package pratibha.co;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pratibha.co.pratibha.repository.KeyGen;
 
 @SpringBootApplication
 public class Application {
+    private static KeyGen keygen;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		keygen.generateAllKeys();
 	}
-
+	@Autowired
+	public void setKeygen(KeyGen key){
+		Application.keygen = key;
+	}
 }
 
--- src/main/java/pratibha/co/pratibha/repository/impl/KeyGenImpl.java
+++ src/main/java/pratibha/co/pratibha/repository/impl/KeyGenImpl.java
@@ -1,14 +1,28 @@
 package pratibha.co.pratibha.repository.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import pratibha.co.pratibha.Permutation;
 import pratibha.co.pratibha.repository.KeyGen;
 
 import java.util.Set;
-
+@Repository
 public class KeyGenImpl implements KeyGen{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     private static final String RANDOM_STRING = "abcde12345";
     public void generateAllKeys(){
         Set<String> keys =  Permutation.findPermutations(RANDOM_STRING, 4);
+        try {
+            for(String key : keys){
+                jdbcTemplate.update(
+                        "INSERT INTO available_urls(short_url) VALUES (?)", key);
+            }
+        } catch (DataAccessException ex){
+            ex.printStackTrace();
+        }
 
     }
 }
